ce194730279538e48392bb2b6cfaacc95e0df90a
Deirdre Storck <deirdre.storck@gmail.com>
2016-05-19 12:23:37 -0700

    refactored show page a bit to make methods more similar to index

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -3,20 +3,36 @@ require_relative "../../lib/TunesTakeoutWrapper"
 
 class SuggestionsController < ApplicationController
 
+
   def show
+
     @search = TunesTakeoutWrapper.search(params[:term], params[:limit])
-    @music = Music.create_music_array(@search)
-    @restaurant_id_array = Food.restaurant_id_array(@search)
-    # @music = reject_playlists(@music)
-    @music_stuff = []
-    @restaurants = Food.restaurant_instances(@restaurant_id_array)
-    @music.each do |id, type|
-      @music_stuff << Music.music_search(id, type)
+    @suggestions = @search["suggestions"]
+    @pairings = []
+    @suggestions.each do |suggestion|
+      restaurant = Food.find_restaurant(suggestion["food_id"])
+      music = Music.music_search(suggestion["music_type"], suggestion["music_id"])
+      @pairings << [music, restaurant, suggestion["id"]]
     end
-    # @music_stuff.first
-    @pairings = @music_stuff.zip(@restaurants)
     @favorites = TunesTakeoutWrapper.favorite_ids(current_user.uid)
+
+
+
   end
+  # def show
+  #   @search = TunesTakeoutWrapper.search(params[:term], params[:limit])
+  #   @music = Music.create_music_array(@search)
+  #   @restaurant_id_array = Food.restaurant_id_array(@search)
+  #   # @music = reject_playlists(@music)
+  #   @music_stuff = []
+  #   @restaurants = Food.restaurant_instances(@restaurant_id_array)
+  #   @music.each do |id, type|
+  #     @music_stuff << Music.music_search(id, type)
+  #   end
+  #   # @music_stuff.first
+  #   @pairings = @music_stuff.zip(@restaurants)
+  #   @favorites = TunesTakeoutWrapper.favorite_ids(current_user.uid)
+  # end
 
   def index
     @top_twenty = TunesTakeoutWrapper.top_twenty
