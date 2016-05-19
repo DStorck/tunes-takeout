ba55bd26a50ef9b81f7821757d299ac6ac1db8ad
Deirdre Storck <deirdre.storck@gmail.com>
2016-05-19 14:34:35 -0700

    refactored takeout wrapper to be less messy"

--- lib/tunes_takeout_wrapper.rb
+++ lib/tunes_takeout_wrapper.rb
@@ -9,31 +9,23 @@ FAV_URL = "https://tunes-takeout-api.herokuapp.com/"
     @response = HTTParty.get(BASE_URL + "#{@search_term}" + "&limit="+ "#{@limit}")
   end
 
-  def self.top_twenty
+  def self.top_twenty #returns array of music and food info
     @initial_response = HTTParty.get("http://tunes-takeout-api.herokuapp.com/v1/suggestions/top?limit=20")
     return [] if @initial_response.nil?
     @tat_ids = @initial_response["suggestions"]
-    @top_pairings = []
-    @tat_ids.each do |suggestion_id|
-      @top_pairings << HTTParty.get(FAV_URL + "/v1/suggestions/" + "#{suggestion_id}")
-    end
-    @top_twenty_array_of_hashes = []
-    @top_pairings.each do |info|
-      @top_twenty_array_of_hashes << info['suggestion']
-    end
-    @top_twenty_array_of_hashes
+    @top_twenty_array_of_hashes = self.suggestion_ids_into_info_array(@tat_ids)
   end
 
-  def self.favorite(userid , id)
+  def self.favorite(userid , id) #this favorites a pairing
     HTTParty.post(FAV_URL + "/v1/users/#{userid}/favorites", body: { "suggestion": "#{id}" }.to_json )
   end
 
-  def self.favorite_ids(userid)
+  def self.favorite_ids(userid) #collects favorite ids
     @response = HTTParty.get(FAV_URL + "/v1/users/#{userid}/favorites")
     @favorites = @response["suggestions"]
   end
 
-  def self.suggestion_ids_into_info_array(sug_id_array)
+  def self.suggestion_ids_into_info_array(sug_id_array) #turns suggestion ids into hashes of food and music info
     @info = []
     sug_id_array.each do |suggestion_id|
       @info << HTTParty.get(FAV_URL + "/v1/suggestions/" + "#{suggestion_id}")
